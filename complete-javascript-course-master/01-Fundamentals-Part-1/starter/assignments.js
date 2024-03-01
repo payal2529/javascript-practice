11e304d94f938315bab45af3146d8f8983048e32
payal2529 <payalp2529@gmail.com>
2024-03-01 14:34:47 +0530

    challenge 2nd completed

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -7,36 +7,31 @@ console.log(continent);
 console.log(population);
 
 // Coding Exercise 1: CHALLENGE #1------------------
-// for first data value-----
-let johnMass = 92; 
-let johnHeight = 1.95;
-let markMass = 78;
-let markHeight = 1.69;
-//  BMI = mass / height ** 2 = mass / (height * height)
-const johnBMI1 = johnMass / johnHeight ** 2
-console.log(johnBMI1);
 
-const markBMI1 = markMass / markHeight ** 2
-console.log(markBMI1);
-
-let markHigherBMI = markBMI1 > johnBMI1
-console.log(markHigherBMI);
+// let johnMass = 92; 
+// let johnHeight = 1.95;
+// let markMass = 78;
+// let markHeight = 1.69;
+// const johnBMI1 = johnMass / johnHeight ** 2
+// const markBMI1 = markMass / markHeight ** 2
+// let markHigherBMI = markBMI1 > johnBMI1
+// console.log(johnBMI1, markBMI1, markHigherBMI);
 
 // -----------------------------or-------------------------
 
-// let massMark = 78;
-// let heightMark = 1.69;
-// let massJohn = 92;
-// let heightJohn = 1.95;
-// let BMIMark = massMark / (heightMark * heightMark);
-// let BMIJohn = massJohn / (heightJohn * heightJohn);
-// let markHigherBMI = BMIMark > BMIJohn;
-// console.log(BMIMark, BMIJohn, markHigherBMI);
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+let markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIMark, BMIJohn, markHigherBMI);
 
-// ---------------------for second given data value ------
+// Coding Exercise 2: CHALLENGE #2
 
-johnMass = 85; 
-johnHeight = 1.76;
-markMass = 95;
-markHeight = 1.88;
-console.log(markHigherBMI = ( markMass / markHeight ** 2) > (johnMass / johnHeight ** 2))
+if(BMIMark > BMIJohn){
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else{
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
