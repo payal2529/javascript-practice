a4d2a9f80788c857c660d7cd3555166b0cedcef4
payal2529 <payalp2529@gmail.com>
2024-03-07 17:39:19 +0530

    Coding Exercise 3: CHALLENGE #3--------Topic :- Type Conversion and Coercion, Truthy and Falsy Values, Equality Operators: == vs. ===, Boolean Logic, Logical Operators

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -6,7 +6,7 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
-// Coding Exercise 1: CHALLENGE #1------------------
+// Coding Exercise 1: CHALLENGE #1---------topic:- basic operators--------
 
 // let johnMass = 92; 
 // let johnHeight = 1.95;
@@ -28,10 +28,26 @@ let BMIJohn = massJohn / (heightJohn * heightJohn);
 let markHigherBMI = BMIMark > BMIJohn;
 console.log(BMIMark, BMIJohn, markHigherBMI);
 
-// Coding Exercise 2: CHALLENGE #2
+// Coding Exercise 2: CHALLENGE #2---------Topic :- strings and template literals, If/else statements----------
 
 if(BMIMark > BMIJohn){
     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
 } else{
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 }
+
+// Coding Exercise 3: CHALLENGE #3--------Topic :- Type Conversion and Coercion, Truthy and Falsy Values, Equality Operators: == vs. ===, Boolean Logic, Logical Operators------
+
+const scoreDolphins = (96 + 108 + 89)/3;
+const scoreKoalas = (88 + 91 + 110)/3;
+console.log(scoreDolphins, scoreKoalas);
+
+if(scoreDolphins > scoreKoalas) {
+    console.log("Dolphin win the trophy");
+}
+  else if( scoreKoalas > scoreDolphins){
+      console.log("Koalas win the trophy");
+  }
+  else{
+      console.log("Both win the trophy");
+  }
