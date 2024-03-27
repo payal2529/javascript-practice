f0ad800a6c90dd0f0ef949252c550c98a8fbcf1e
payal2529 <payalp2529@gmail.com>
2024-03-27 12:38:44 +0530

    CHALLENGE #2 completed

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -25,3 +25,33 @@ scoreDolphins = calcAverage(85,54,41);
 scoreKoalas = calcAverage(23,34,27);
 console.log(scoreDolphins, scoreKoalas);
 checkWinner(scoreDolphins, scoreKoalas);
+
+// Coding Exercise 6: CHALLENGE #2///
+// 1st task
+// by using if/else //////
+// const calcTip = function(bill){
+//     if( bill >= 50 && bill <= 300){
+//         return (bill * 15)/100;
+//     } else {
+//         return (bill * 20)/100;
+//     }
+// }
+//////////////////////// by using ternery operators //////////////////////////////
+const calcTip = function(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+
+ console.log(calcTip(100));
+
+//  2nd task
+ const bills = [125, 555, 44];
+ 
+//  3rd task
+ const tips = [calcTip(bills[0]) , calcTip(bills[1]) , calcTip(bills[2])];
+ 
+//   4th task
+const totals = [bills[0] + tips[0] ,
+               bills[1] + tips[1] ,
+               bills[2] + tips[2]];
+console.log(bills , tips);
+console.log(totals);
